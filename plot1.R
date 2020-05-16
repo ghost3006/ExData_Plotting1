dbccf601c9c55d0d37ba412a98bd81f3bc3232c5
ghost30 <prateekjha30@gmail.com>
2020-05-16 18:58:14 +0530

    comments added

--- plot1.R
+++ plot1.R
@@ -12,8 +12,10 @@ table$Sub_metering_1<- as.numeric(table$Sub_metering_1)
 table$Sub_metering_2<- as.numeric(table$Sub_metering_2)
 table$Sub_metering_3<- as.numeric(table$Sub_metering_3)
 
+#Taking subset of 2 days only
 subdata <- subset(table, Date =="2007-02-01" | Date == "2007-02-02")
 
+#creating and saving the histogram as plot1.png
 png(filename = "plot1.png", width = 480, height = 480)
 hist(subdata$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
